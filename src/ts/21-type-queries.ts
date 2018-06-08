dcc0532246b993385be522a9ab945c4f643ae9b7
Peter Kassenaar <info@kassenaar.com>
2018-06-08 11:26:17 +0200

    Updated Type Query example

--- src/ts/21-type-queries.ts
+++ src/ts/21-type-queries.ts
@@ -9,7 +9,7 @@ namespace type_queries {
 
 	/*********************************** */
 	// 2. With TypeScript, the typeof operator acts slightly different
-	// If you use it for type declarations, it's called a Type Query.
+	// If you use it for type declarations, it's called a *Type Query*.
 
 	// Lets say we have a anonymous type/object literal as such
 	const employee = {
@@ -23,13 +23,13 @@ namespace type_queries {
 
 	let person: Person = {
 		name: 'Brandon',
-		age: 19
+		age: 9
 	};
 
 	// Or, using an inline syntax:
 	let anotherPerson: typeof employee = {
 		name: 'Sansa',
-		age: 24
+		age: 11
 	};
 
 	/*********************************** */
@@ -48,14 +48,47 @@ namespace type_queries {
 	// 4. Create a lookup function:
 	//    ask for a property of an object
 
+	// 4a. Old-skool ES5-function. This works, but in a type unsafe way.
 	// function getProperty(obj: object, key: string) {
-	// 	return obj[key]; // This works, but in a type unsafe way
+	// 	return obj[key];
 	// }
-	// function getProperty<T, K extends keyof T>(obj: T, key: K) {
-	//	// K is a *subtype* of T
-	// 	return obj[key]; // type safe way
+
+	// 4b. TypeScript way, K is a *subtype* of T. It HAS to have the members of type T.
+	// function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+	// 	return obj[key];
 	// }
 
 	// const personName = getProperty(person, 'name'); // 'name' HAS to exsist in the other object, to work
 	// console.log(personName); // 'Brandon'
+
+	/*********************************** */
+	// 5. Real life example, Sort an array on specific key,
+	//  but make sure the key exists.
+	/*********************************** */
+
+	// 5a. Data
+	// const personsArray: typeof person[] = [
+	// 	{ name: 'Eddard', age: 45 },
+	// 	{ name: 'Sansa', age: 11 },
+	// 	{ name: 'Arya', age: 9 },
+	// 	{ name: 'Robb', age: 15 },
+	// 	{ name: 'Rickon', age: 4 },
+	// 	{ name: 'Catelyn', age: 36 }
+	// ];
+
+	// // 5b. The orderBy helperfunction, using the Index Access Type notation.
+	// function orderBy<T, K extends keyof T>(objArray: T[], prop: K): T[] {
+	// 	return objArray.sort((obj1: T, obj2: T) => {
+	// 		if (obj1[prop] > obj2[prop]) {
+	// 			return 1;
+	// 		} else if (obj1[prop] < obj2[prop]) {
+	// 			return -1;
+	// 		}
+	// 		return 0;
+	// 	});
+	// }
+
+	// // 5c. Implementation
+	// const sortedPersons = orderBy(personsArray, 'name');
+	// console.log(sortedPersons);
 } // end namespace
