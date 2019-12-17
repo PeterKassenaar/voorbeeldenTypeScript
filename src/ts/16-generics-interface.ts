f061840d2d497ddbcf1b170652b8663ca1eb0c7b
Peter Kassenaar <info@kassenaar.com>
2019-12-17 19:46:38 +0100

    Updated generics-interface example

--- src/ts/16-generics-interface.ts
+++ src/ts/16-generics-interface.ts
@@ -1,7 +1,7 @@
 // generics-interface.ts - comment in/out the sections in this file
 namespace generics_interface_example {
   //************************************************** */
-  // 0. Set up soime interfaces and some data.
+  // 0. Set up some interfaces and some data.
   interface Employee {
     name: string;
     age: number;
@@ -9,7 +9,7 @@ namespace generics_interface_example {
 
   let admin: Employee = {
     name: 'Johan',
-    age: 34
+    age: 29
   };
 
   let manager: Employee = {
@@ -18,7 +18,9 @@ namespace generics_interface_example {
   };
 
   //************************************************** */
-  // 1. No type information. This works, but gives you no type safety
+  // 1. No type information. This works, but
+  // gives you no type safety.
+  //************************************************** */
   function echoEmployees(person) {
     return person;
   }
@@ -26,21 +28,25 @@ namespace generics_interface_example {
   let foo = echoEmployees(admin); // type of foo is now 'any'
   console.log(foo);
   //************************************************** */
-  // 2. With Generic: now we have at least type information on property 'person'.
+  // 2. With Generic: now we have at least type
+  // information on property 'person'.
+  //************************************************** */
   // function echoEmployees<T>(person: T): T {
   //   return person;
   // }
-
+  //
   // let foo = echoEmployees(admin); // type of foo is now Employee, but still no type safety inside echoEmployees
   // console.log(foo);
 
   //************************************************** */
-  // 3. With Generic and Type Constraint: we have type safety inside the function
+  // 3. With Generic and Type Constraint: we have
+  // type safety *inside* the function
+  //************************************************** */
   // function echoEmployees<T extends Employee>(person: T): T {
-  //   console.log('Name of the person: ', person.);
+  //   // console.log('Name of the employee: ', person.);
   //   return person;
   // }
-
+  //
   // let foo = echoEmployees(admin); // type of foo is now of type Employee, including type safety inside generic function.
   // console.log(foo);
 }
