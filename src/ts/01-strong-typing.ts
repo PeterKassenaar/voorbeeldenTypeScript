02f4240b1d93ee063ac4e703fa9a7a68b43f8d62
Peter Kassenaar <info@kassenaar.com>
2022-09-14 20:46:59 +0200

    Updated some examples and package-lock.json

--- src/ts/01-strong-typing.ts
+++ src/ts/01-strong-typing.ts
@@ -1,5 +1,6 @@
 //********************
-// Plain ES5-JavaScript
+// Example on Strongly typing, one of the benefits of TypeScript.
+// in Plain ES5-JavaScript - NO Advantages here!
 //********************
 let myVar;
 myVar = 42;
@@ -11,7 +12,9 @@ myVar = {
 console.log('myVar is nu : ', myVar); // ALL valid!
 
 //*************************
-// TypeScript - Strongly typed variables
+// TypeScript - Strongly typed variables.
+// We get development time errors if we use for example the wrong values for variables.
+// Also see tsconfig.json - it has the simplest configuration/compilation settings possible.
 //*************************
 let myVar1: number;
 // myVar1 = 'Hello World '; // Error 'Assigned expression type string is not assignable to type number'
@@ -24,3 +27,7 @@ myVar4 = {
     lastName: 'Kassenaar'
 };
 console.log('myVar4: ', myVar4);
+
+// running this example?
+// - In the browser: Open 'html/01-strong-typing.html'. It has a reference to the compiled JavaScript file.
+// - In Node.js: cd to the correct directory (like 'cd src/js') and use command 'node 01-strong-typing.js'
